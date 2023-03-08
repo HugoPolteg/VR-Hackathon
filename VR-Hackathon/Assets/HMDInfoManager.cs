5f9add438a46526a76f6ced190792e351e1a4ab2
HugoPolteg <hugopolteg@outlook.com>
2023-03-08 16:55:51 +0100

    Added proper scene

--- VR-Hackathon/Assets/HMDInfoManager.cs
+++ VR-Hackathon/Assets/HMDInfoManager.cs
@@ -1,13 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR;
 
 public class HMDInfoManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Is device active" + XRSettings.isDeviceActive);
+        Debug.Log("Device name is " + XRSettings.loadedDeviceName);
+        if (!XRSettings.isDeviceActive)
+        {
+            Debug.Log("No headset detected");
+        }
+        else if(XRSettings.isDeviceActive && (XRSettings.loadedDeviceName == "Mock HMD" || XRSettings.loadedDeviceName == "MockHMD Display")) 
+        {
+            Debug.Log("using Mock HMD");
+        }
+        else
+        {
+            Debug.Log("using a " + XRSettings.loadedDeviceName + " headset");
+        }
     }
 
     // Update is called once per frame
